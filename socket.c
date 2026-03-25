f5b3763413b4240edf235512c4f6fc2fd359de7c
chilly663 <2248765177@qq.com>
2026-03-25 21:42:05 +0800

    增加了socket中的代码注释

--- socket.c
+++ socket.c
@@ -32,14 +32,16 @@ int Socket(const char *host, int clientPort)
     unsigned long inaddr;
     struct sockaddr_in ad;
     struct hostent *hp;
-    
-    memset(&ad, 0, sizeof(ad));
-    ad.sin_family = AF_INET;
+    //sockaddr_in 的结构
+    //sin_family 是族域名，区分ipv4和ipv6的应该是
+    memset(&ad, 0, sizeof(ad));//清空域名结构体，为了防止垃圾无效的数字
+    ad.sin_family = AF_INET;  //设置ipv4，这里用ipv4纯是因为当时没有ipv6，之后试试嫩不能改成ipv6兼容
 
-    inaddr = inet_addr(host);
-    if (inaddr != INADDR_NONE)
+
+    inaddr = inet_addr(host);//将域名变成32位存储，但是inet_addr貌似不支持ipv6，而且不安全
+    if (inaddr != INADDR_NONE)//INADDR_NONE是用来判断是否合法，但是对于255.255.255.255会出问题
         memcpy(&ad.sin_addr, &inaddr, sizeof(inaddr));
-    else
+    else//可能主机
     {
         hp = gethostbyname(host);
         if (hp == NULL)
